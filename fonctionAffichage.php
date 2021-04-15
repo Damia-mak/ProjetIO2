fe0fd55c0209cafe3b6b00a27afbb1572a431c0b
Ewen Glasziou <ewen.glasziou@gmail.com>
2021-04-15 15:33:06 +0200

    Update fonctionAffichage.php

--- fonctionAffichage.php
+++ fonctionAffichage.php
@@ -1,15 +1,15 @@
 <?php
 function entete($titre,$css){
-echo "<!DOCTYPE>";
-echo "<html lang=\"fr\">";
-echo "<head>";
-echo "<meta charset=\"utf-8\">";
-echo "<link rel=\"stylesheet\" href=\"$css?1\">","<title>$titre</title>","</head>";
-echo "<body>";
+echo "<!DOCTYPE>","\n";
+echo "<html lang=\"fr\">","\n";
+echo "<head>","\n";
+echo "<meta charset=\"utf-8\">","\n";
+echo "<link rel=\"stylesheet\" href=\"$css?1\">","<title>$titre</title>","</head>","\n";
+echo "<body>","\n";
 }
 function basDePage(){
-    echo "</body>";
-    echo " </html>";
+    echo "</body>","\n";
+    echo " </html>","\n";
 }
 
 
