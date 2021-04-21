6256186bb8e5d78e2fad604578edd309f144a5a9
Ewenou <82279936+Ewenou@users.noreply.github.com>
2021-04-21 18:49:04 +0200

    Delete test.php

--- test.php
+++
@@ -1,7 +0,0 @@
-<?php 
-if (str_length($_REQUEST['passwd']) < 6 || $_REQUEST['passwd'] != $_REQUEST['passwd1']) echo "mot de passe incorrecte !",
-"<br>","<a href=\"formulaire.php\">Retourner au formulaire ici :)</a>";
-else if($_REQUEST['prenom'] == "Votre prénom.") echo "Veuillez inscrire votre prénom",
-    "<br>","<a href=\"formulaire.php\">Retourner au formulaire ici :)</a>";
-    else echo $_REQUEST['prenom'];
-?>
